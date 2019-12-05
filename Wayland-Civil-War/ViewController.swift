4b78bc2ac38879c20afdfdd10bab48c96556f996
jaspershoe <jasper_hsu@student.wayland.k12.ma.us>
2019-12-05 15:00:21 -0500

    Initial Commit

---
+++ Wayland-Civil-War/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wayland-Civil-War
+//
+//  Created by Jasper_Hsu on 12/5/19.
+//  Copyright © 2019 HACS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wayland-Civil-WarTests/Wayland_Civil_WarTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wayland_Civil_WarTests.swift
+//  Wayland-Civil-WarTests
+//
+//  Created by Jasper_Hsu on 12/5/19.
+//  Copyright © 2019 HACS. All rights reserved.
+//
+
+import XCTest
+@testable import Wayland_Civil_War
+
+class Wayland_Civil_WarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
