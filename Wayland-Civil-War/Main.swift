008540b6bbd5207d9a31633478890f928dd08d05
Rob <remersong@gmail.com>
2019-12-13 12:52:28 -0500

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 3bbe3af31a3ba58d17b7dec02f6441908c51475b, reversing
    changes made to 47477da088350037d6eb905a0f42ca7c25a9b284.

--- Wayland-Civil-War/Main.swift
+++ Wayland-Civil-War/Main.swift
@@ -7,30 +7,14 @@
 //
 
 import UIKit
-import MapKit
 
 class ViewController: UIViewController {
-    
-    let locationManager = CLLocationManager()
 
-    @IBOutlet weak var mapView: MKMapView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        let wayland = CLLocation(latitude: 42.3626, longitude: -71.3614)
-        let regionRadius = 2000.0
-        let region = MKCoordinateRegion(center: wayland.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        
-        mapView.setRegion(region, animated: true)
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
     }
+
+
 }
 
-extension ViewController: CLLocationManagerDelegate {
-    
-}
