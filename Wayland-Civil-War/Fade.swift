09209e44e990604a5fef7e057cd1c5d451dc6d3b
alex_kashian <alex_kashian@student.wayland.k12.ma.us>
2020-03-09 13:40:42 -0400

    super cool commit

---
+++ Wayland-Civil-War/Fade.swift
@@ -0,0 +1,15 @@
+//
+//  Fade.swift
+//  Wayland-Civil-War
+//
+//  Created by Alex_Kashian on 2/13/20.
+//  Copyright © 2020 HACS. All rights reserved.
+//
+
+import UIKit
+
+class Fade: UIView {
+
+
+
+}
